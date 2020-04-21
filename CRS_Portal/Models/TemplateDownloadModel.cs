7c98cf7204e4220d03d58437614e03a8c7f3bd84
Arunkumar.S <arunkumar.s@macroinfotech.co.uk>
2020-04-21 19:32:56 +0530

    CRS Template files added in to project

--- CRS_Portal/Models/TemplateDownloadModel.cs
+++ CRS_Portal/Models/TemplateDownloadModel.cs
@@ -7,5 +7,11 @@ namespace CRS_Portal.Models
 {
     public class TemplateDownloadModel
     {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TemplateFilePath { get; set; }
+        public string LastYearLookupData { get; set; }
+        public bool IsActive { get; set; }
     }
 }
