5fa48a44ab11cf49ebf8734ffb9b199f32774d2c
Arunkumar.S <arunkumar.s@macroinfotech.co.uk>
2020-04-21 19:23:04 +0530

    Audit History, Individula lookup and TemplateDownlad views, controllers added in to project

---
+++ CRS_Portal/Entity/IndividualLookupDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRS_Portal.Entity
+{
+    public class IndividualLookupDbContext
+    {
+    }
+}
