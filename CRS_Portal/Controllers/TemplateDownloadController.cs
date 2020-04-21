c88654d8dbb14954bd4a00abb1ac2e7232d0f2d1
Arunkumar.S <arunkumar.s@macroinfotech.co.uk>
2020-04-21 19:39:48 +0530

    Template download DbContext added

--- CRS_Portal/Controllers/TemplateDownloadController.cs
+++ CRS_Portal/Controllers/TemplateDownloadController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CRS_Portal.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CRS_Portal.Controllers
 {
@@ -11,6 +13,30 @@ namespace CRS_Portal.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult LoadTemplateGrid()
+        {
+            try
+            {
+                List<TemplateDownloadModel> lstModel = new List<TemplateDownloadModel>();
+
+                TemplateDownloadModel t1Model = new TemplateDownloadModel();
+                //t1Model.ID = 1;
+                //t1Model.IsActive = true;
+                //t1Model.Title = "Individual Lookup";
+                //t1Model.Description = "Download Individual Lookup file";
+                //t1Model.
+
+
+                var sa = new JsonSerializerSettings();
+                return Json(lstModel, sa);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
     }
 }
