62b703eebc45c7174d4a1c160cbb277bb5a0ab48
Jasmine <jasmine.john@macroinfotech.co.uk>
2020-04-22 09:16:34 +0530

    Download Template, Modal Add Design initial checkin

--- CRS_Portal/Controllers/EntityLookupController.cs
+++ CRS_Portal/Controllers/EntityLookupController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CRS_Portal.Entity;
 using CRS_Portal.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -12,10 +13,11 @@ namespace CRS_Portal.Controllers
     public class EntityLookupController : Controller
     {
         EntityLookupDbContext objEntitytDbContext;
+        private IHostingEnvironment _hostingEnv;
 
-        public EntityLookupController()
+        public EntityLookupController(IHostingEnvironment hostingEnv)
         {
-            
+            _hostingEnv = hostingEnv;
         }
 
         public IActionResult Index()
@@ -44,6 +46,21 @@ namespace CRS_Portal.Controllers
             catch (Exception ex)
             {
                 return Json(new { success = false, message = "The server has encountered an unexpected internal error. Please try again later." });
+            }
+        }
+
+        public ActionResult DownloadCRSEntityCustomerTemplate()
+        {
+            try
+            {
+                string path = _hostingEnv.WebRootPath + "\\TemplateFiles\\CRS_Entity_Lookup_Template.xlsx";
+                byte[] fileBytes = System.IO.File.ReadAllBytes(path);
+                string fileName = "CRS_Entity_Lookup_Template.xlsx";
+                return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+            }
+            catch (Exception ex)
+            {
+                return File("", System.Net.Mime.MediaTypeNames.Application.Octet, "");
             }
         }
     }
